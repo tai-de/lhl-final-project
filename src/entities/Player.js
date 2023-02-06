d6320134053561c045eb006c363b37465fa2afb0
Tai (he/him) <116750472+tai-de@users.noreply.github.com>
2023-02-06 15:48:12 -0700

    implemented player anims and jumping

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -20,6 +20,9 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
   init() { // Initialize all of the player variables/logic
     this.gravity = 500;
     this.playerSpeed = 200;
+    this.jumpCount = 0;
+
+    this.lastDirection = Phaser.Physics.Arcade.FACING_RIGHT;
 
     this.setSize(20, 30);
     this.setOffset(10, 30);
@@ -34,17 +37,36 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
   }
 
   update() {
-    const { left, right } = this.cursors;
+    const { left, right, space } = this.cursors;
+    const onFloor = this.body.onFloor();
 
+    // Checking if space was just pressed to prevent duplicate
+    const isSpaceJustDown = Phaser.Input.Keyboard.JustDown(space);
+
+    // Regular L/R movement
     if (left.isDown) {
       this.setVelocityX(-this.playerSpeed);
+      this.setFlipX(true);
+      this.lastDirection = Phaser.Physics.Arcade.FACING_LEFT;
     } else if (right.isDown) {
       this.setVelocityX(this.playerSpeed);
+      this.setFlipX(false);
+      this.lastDirection = Phaser.Physics.Arcade.FACING_RIGHT;
     } else {
       this.setVelocityX(0);
     }
 
-    this.play('player-idle', true);
+    // Jumping only when on the floor
+    if (isSpaceJustDown && onFloor) {
+      this.setVelocityY(-this.playerSpeed * 1.75);
+    }
+
+    // Player animations
+    onFloor ? 
+      this.body.velocity.x !== 0 ?
+        this.play('player-run', true) : this.play('player-idle', true) :
+      this.play('player-jump', true);
+    
   }
 
 }
