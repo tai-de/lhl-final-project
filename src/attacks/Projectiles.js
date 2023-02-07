13849873f4ae89bc1e2365802488dfc61dd763ce
Tai (he/him) <116750472+tai-de@users.noreply.github.com>
2023-02-07 15:33:18 -0700

    update Projectiles to change directions

--- src/attacks/Projectiles.js
+++ src/attacks/Projectiles.js
@@ -5,19 +5,27 @@ export default class Projectiles extends Phaser.Physics.Arcade.Group {
   constructor(scene) {
     super(scene.physics.world, scene);
 
-   this.createMultiple({
-    frameQuantity: 5,
-    active: false,
-    visible: false,
-    key: 'fireball1',
-    classType: Projectile 
-   });
+    this.createMultiple({
+      frameQuantity: 5,
+      active: false,
+      visible: false,
+      key: 'fireball1',
+      classType: Projectile
+    });
   }
 
-  shootProjectile(initiator){
+  shootProjectile(initiator) {
     const projectile = this.getFirstDead(false);
 
     if (!projectile) { return; }
+
+    if (initiator.lastDirection === Phaser.Physics.Arcade.FACING_RIGHT) {
+      projectile.speed = Math.abs(projectile.speed);
+      projectile.setFlipX(false);
+    } else {
+      projectile.speed = -Math.abs(projectile.speed);
+      projectile.setFlipX(true);
+    }
 
     projectile.shoot(initiator.x, initiator.y);
   }
