b21b73eeca6b6570ccb92098607bd81a6218c867
Tai (he/him) <116750472+tai-de@users.noreply.github.com>
2023-02-07 11:40:12 -0700

    fix Enemy flip to prevent stuttering / add delay to turns

--- src/entities/Enemy.js
+++ src/entities/Enemy.js
@@ -19,6 +19,7 @@ export default class Enemies extends Phaser.Physics.Arcade.Sprite {
   init() {
     this.gravity = 500;
     this.speed = 30;
+    this.timeFromLastTurn = 0;
     this.body.setGravityY(this.gravity);
     this.setCollideWorldBounds(true);
     this.setOrigin(0.5, 1);
@@ -35,10 +36,11 @@ export default class Enemies extends Phaser.Physics.Arcade.Sprite {
   update(time, delta) {
     this.setVelocityX(this.speed);
     const { ray, hasHits } = this.rayCast(this.body, this.platformCollidersLayer);
-    if (!hasHits) {
+    if (!hasHits && this.timeFromLastTurn + 100 < time) {
       // console.log('Has Hits');
       this.setFlipX(!this.flipX);
-      this.setVelocityX(this.speed = -this.speed) ;
+      this.setVelocityX(this.speed = -this.speed);
+      this.timeFromLastTurn = time;
     }
     this.rayGraphics.clear();
     this.rayGraphics.strokeLineShape(ray);
