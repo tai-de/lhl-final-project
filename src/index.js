38227f39282b07ba2b97175c583496d7fc510d46
Tai (he/him) <116750472+tai-de@users.noreply.github.com>
2023-02-06 17:51:04 -0700

    update with maxWidth & calculations for mapOffset

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import Phaser from 'phaser';
 import PreloadScene from './scenes/Preload';
 import PlayScene from './scenes/Play';
 
-const WIDTH = 1280;
+const WIDTH = document.body.offsetWidth;
 const HEIGHT = 600;
+const MAPWIDTH = 1600;
 
 // Shared configuration object will be passed through to all scenes
 const SHARED_CONFIG = {
+  mapOffset: MAPWIDTH > WIDTH ? MAPWIDTH - WIDTH : 0,
   width: WIDTH,
   height: HEIGHT,
   debug: true, // Change this to true to enable phaser debugging
