0616f50862236ab23fcf7b731031739c110d66fe
Tai (he/him) <116750472+tai-de@users.noreply.github.com>
2023-02-08 13:07:22 -0700

    fix Snake so lastDirection is tracked for Projectile dir

--- src/entities/Snake.js
+++ src/entities/Snake.js
@@ -18,6 +18,7 @@ export default class Snake extends Enemy {
     this.setOffset(8, 15);
     this.timeFromLastAttack = null;
     this.attackDelay = Phaser.Math.Between(1000, 3000);
+    this.lastDirection = null;
 
     this.projectiles = new Projectiles(this.scene, 'iceball1');
   }
@@ -25,12 +26,19 @@ export default class Snake extends Enemy {
   update(time, delta) {
     super.update(time, delta);
 
-    if(this.timeFromLastAttack + this.attackDelay <= time){
+    if (!this.active) { return; }
+
+    if (this.body.velocity.x > 0) {
+      this.lastDirection = Phaser.Physics.Arcade.FACING_RIGHT;
+    } else {
+      this.lastDirection = Phaser.Physics.Arcade.FACING_LEFT;
+    }
+
+    if (this.timeFromLastAttack + this.attackDelay <= time) {
       this.projectiles.shootProjectile(this);
       this.timeFromLastAttack = time;
-      this.attackDelay = Phaser.Math.Between(1000, 3000);   
+      this.attackDelay = Phaser.Math.Between(1000, 3000);
     }
-    if (!this.active) { return; }
 
     if (this.isPlayingAnims('snake-hurt')) { return; }
 
@@ -42,4 +50,4 @@ export default class Snake extends Enemy {
     this.play('snake-hurt', true);
   }
 
-} 
+}
