bfd1ff97f64b34b6a171798e62008955e467cc89
Tai (he/him) <116750472+tai-de@users.noreply.github.com>
2023-02-07 20:42:56 -0700

    update MeleeWeapon to flip with character and fix size

--- src/attacks/MeleeWeapon.js
+++ src/attacks/MeleeWeapon.js
@@ -7,26 +7,45 @@ export default class MeleeWeapon extends Phaser.Physics.Arcade.Sprite {
     // Binds 'this' context to the scene
     scene.add.existing(this);
     scene.physics.add.existing(this);
-    
+
     // this.setOrigin(0.5, 1);
     this.attackSpeed = 1000;
     this.damage = 10;
     this.weaponAnim = weaponName + '-anim';
-    
+
+    this.activateWeapon(false);
+    this.wielder = null;
+
     this.on('animationcomplete', (animation) => {
-      if(animation.key === this.weaponAnim){
+      if (animation.key === this.weaponAnim) {
         this.activateWeapon(false);
         this.body.reset(0, 0);
       }
-    })
+    });
 
   }
+
+  preUpdate(time, delta) {
+    super.preUpdate(time, delta);
+
+    if (!this.active) { return; }
+
+    if (this.wielder.lastDirection === Phaser.Physics.Arcade.FACING_RIGHT) {
+      this.setFlipX(false);
+      this.setOffset(18, 0); // Offset of the hit box / debug
+      this.body.reset((this.wielder.x + this.wielder.width / 2) - 11, this.wielder.y);
+    } else {
+      this.setFlipX(true); 
+      this.setOffset(0, 0);
+      this.body.reset((this.wielder.x - this.wielder.width / 2) + 10, this.wielder.y);
+    }
+  }
+
 
   swing(wielder) {
     this.wielder = wielder;
     this.activateWeapon(true);
-    this.body.reset(wielder.x, wielder.y);
-    this.anims.play(this.weaponAnim, true).setOrigin(0.5, 1);
+    this.anims.play(this.weaponAnim).setOrigin(0.5, 1);
   }
 
   activateWeapon(isActive) {
