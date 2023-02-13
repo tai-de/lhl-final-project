da14cc98157c01d8afaff57fad042bbd214fe004
Edward Lee <edward.lee3@telus.com>
2023-02-13 01:02:02 -0500

    Add favicon

--- src/scenes/Preload.js
+++ src/scenes/Preload.js
@@ -52,7 +52,7 @@ export default class Preload extends Phaser.Scene {
                 percentText.setText(parseInt(value * 100) + '%');
                 progressBar.clear();
                 progressBar.fillStyle(0xffffff, 1);
-                progressBar.fillRect(300, 280, 300 * value, 30);
+                progressBar.fillRect(250, 280, 300 * value, 30);
             });
             
             this.load.on('fileprogress', function (file) {
