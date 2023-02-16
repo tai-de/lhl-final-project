9794434007dd93e3cdc8b2bec196b71b839316a1
Tai (he/him) <116750472+tai-de@users.noreply.github.com>
2023-02-16 12:23:09 -0700

    additional changes to ensure levels scene loads correctly

--- src/scenes/Levels.js
+++ src/scenes/Levels.js
@@ -13,8 +13,8 @@ export default class LevelScene extends BaseScene {
 
     const levelsCompleted = localStorage.getItem('levels-completed') || 0;
 
-    for (let i = 0; i < levelsCompleted; i++) {
-      if (i > this.finalLevel) { break; }
+    for (let i = 0; i <= levelsCompleted; i++) {
+      if (i >= this.finalLevel) { break; }
       this.menu.push({
         scene: 'PlayScene',
         text: `Level ${i+1}`,
